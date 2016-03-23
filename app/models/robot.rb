e574c054f4f0e38a3f0f6480c2897c57957f7de2
Sal Espinosa <sespinos@gmail.com>
2016-03-23 07:13:11 -0600

    Initial commit

---
+++ app/models/robot.rb
@@ -0,0 +1,21 @@
+class Robot
+  attr_reader :id,
+              :name,
+              :city,
+              :state,
+              :avatar,
+              :birthdate,
+              :date_hired,
+              :department
+
+  def initialize(args)
+    @id         = args["id"],
+    @name       = args["name"],
+    @city       = args["city"],
+    @state      = args["state"],
+    @avatar     = args["avatar"],
+    @birthdate  = args["birthdate"],
+    @date_hired = args["date_hired"],
+    @department = args["department"]
+  end
+end
---
+++ app/models/robot_directory.rb
@@ -0,0 +1,66 @@
+require 'yaml/store'
+require_relative 'robot'
+
+class RobotDirectory
+  attr_reader :database
+
+  def initialize(database)
+    @database = database
+  end
+
+  def create(robot)
+    database.transaction do
+      database['robots'] ||= []
+      database['total']  ||= 0
+      database['total']   += 1
+      database['robots'] << {"id"         => database['total'],
+                             "name"       => robot[:name],
+                             "city"       => robot[:city],
+                             "state"      => robot[:state],
+                             "avatar"     => robot[:avatar],
+                             "birthdate"  => robot[:birthdate],
+                             "date_hired" => robot[:date_hired],
+                             "department" => robot[:department] }
+    end
+  end
+
+  def update(id, robot)
+    database.transaction do
+      target = database['robots'].detect { |robot| robot["id"] == id }
+      target["name"]       = robot[:name]
+      target["city"]       = robot[:city]
+      target["state"]      = robot[:state]
+      target["avatar"]     = robot[:avatar]
+      target["birthdate"]  = robot[:birthdate]
+      target["date_hired"] = robot[:date_hired]
+      target["department"] = robot[:department]
+    end
+  end
+
+  def delete(id)
+    database.transaction do
+      database['robots'].delete_if { |robot| robot["id"] == id }
+    end
+  end
+
+  def all
+    raw_robots.map { |robot| Robot.new(robot)}
+  end
+
+  def find(id)
+    Robot.new(raw_robot(id))
+  end
+
+
+
+  private
+  def raw_robots
+    database.transaction do
+      database['robots'] || []
+    end
+  end
+
+  def raw_robot(id)
+    raw_robots.find { |robot| robot["id"] == id }
+  end
+end
