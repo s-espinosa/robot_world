77f5070c067ad3e1c440871617902080d28f08db
Sal Espinosa <sespinos@gmail.com>
2016-03-26 15:28:50 -0600

    Update migration to reflect what I'm actually doing

--- db/migrate/001_create_robots.rb
+++ db/migrate/001_create_robots.rb
@@ -1,13 +1,17 @@
 require 'sequel'
 
-database = Sequel.sqlite('db/robot_directory_development.sqlite')
-database.create_table :robots do
-  primary_key :id
-  String      :name
-  String      :city
-  String      :state
-  String      :avatar
-  String      :birthdate
-  String      :date_hired
-  String      :department
+environments = ['test', 'development']
+
+environments.each do |env|
+  database = Sequel.sqlite('db/robot_directory_#{env}.sqlite')
+  database.create_table :robots do
+    primary_key :id
+    String      :name
+    String      :city
+    String      :state
+    String      :avatar
+    String      :birthdate
+    String      :date_hired
+    String      :department
+  end
 end
