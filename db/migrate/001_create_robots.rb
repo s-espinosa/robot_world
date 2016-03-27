b1027e91e2f2a957ee1877293e6edc9c0a21c142
Sal Espinosa <sespinos@gmail.com>
2016-03-27 11:38:52 -0600

    Update to correctly create DBs

--- db/migrate/001_create_robots.rb
+++ db/migrate/001_create_robots.rb
@@ -3,7 +3,7 @@ require 'sequel'
 environments = ['test', 'development']
 
 environments.each do |env|
-  database = Sequel.sqlite('db/robot_directory_#{env}.sqlite')
+  database = Sequel.sqlite("db/robot_directory_#{env}.sqlite")
   database.create_table :robots do
     primary_key :id
     String      :name
